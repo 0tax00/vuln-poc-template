1297591e106a56446c3b208fff6d372f4dcc7d0c
Mateus Pantoja <84558385+0tax00@users.noreply.github.com>
2025-02-12 15:08:45 -0300

    Update localports.ps1

--- localports.ps1
+++ localports.ps1
@@ -1,11 +1,14 @@
 Get-NetTCPConnection -State Listen | ForEach-Object {
-
+    # Use um nome de variável diferente de $PID, que já é reservada
     $procID = $_.OwningProcess
-    
+
+    # Obter o processo (se existir)
     $proc = Get-Process -Id $procID -ErrorAction SilentlyContinue
-    
+
+    # Obter o serviço associado a esse processo (se for um serviço)
     $svc  = Get-WmiObject Win32_Service -Filter "ProcessId = $procID" -ErrorAction SilentlyContinue
 
+    # Criar um objeto customizado com as informações desejadas
     [PSCustomObject]@{
         LocalAddress = $_.LocalAddress
         LocalPort    = $_.LocalPort
